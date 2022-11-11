c3610ba6cd16b75bf689c763b5583d626349d421
Liza <Lizacastdiaz@gmail.com>
2022-11-11 19:04:28 +0100

    Adding the api to the min proyect, adding shot css code and html

---
+++ Script/proyect.js
@@ -0,0 +1,25 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '59799d1fa0msh9950dfefdab022cp1fc48ejsna673230b3b97',
+		'X-RapidAPI-Host': 'ipstack1.p.rapidapi.com'
+	}
+};
+fetch('https://ipstack1.p.rapidapi.com/api.ipstack.com', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+const form = $document.querySelector('#form');
+const $input = document.querySelector('#input')
+
+$form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const {value} = $input
+  if (!value) return
+
+  const ipInfo = await fetch(value)
+})
+
+
